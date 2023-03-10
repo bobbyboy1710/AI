597bec3aca47c3fdfcea8538720d24bfb3eefda4
Dmytro Babich <bobbyboy1710@gmail.com>
2023-03-10 10:31:21 +0200

    Change LR_task_6.py

--- lab1/LR_task_2.py
+++ lab1/LR_task_2.py
@@ -1,13 +1,13 @@
 import numpy as np
 from sklearn import preprocessing
-input_data = np.array([[4.1, -5.9, -3.5], [-1.9, 4.6, 3.9], [-4.2, 6.8, 6.3], [3.9, 3.4, 1.2]])
-data_binarized = preprocessing.Binarizer(threshold=3.2).transform(input_data)
+input_data = np.array([[1.3, -3.9, 6.5], [-4.9, -2.2, 1.3],
+                      [2.2, 6.5, -6.1], [-5.4, -1.4, 2.2]])
+data_binarized = preprocessing.Binarizer(threshold=1.1).transform(input_data)
 data_scaled = preprocessing.scale(input_data)
 data_scaler_minmax = preprocessing.MinMaxScaler(feature_range=(0, 1))
 data_scaled_minmax = data_scaler_minmax.fit_transform(input_data)
 data_normalized_l1 = preprocessing.normalize(input_data, norm='l1')
 data_normalized_l2 = preprocessing.normalize(input_data, norm='l2')
-
 
 
 print('\n Бінаризація: \n', data_binarized)
--- lab1/LR_task_6.py
+++ lab1/LR_task_6.py
@@ -2,13 +2,14 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, accuracy_score
 
 # Зчитуємо дані з файлу та створюємо DataFrame
 data = pd.read_csv('data_metrics.csv')
 
 # Визначаємо змінні X та y для моделі
-X = data.drop('actual_label', axis=1)  # Вибираємо усі змінні, окрім цільової змінної
+# Вибираємо усі змінні, окрім цільової змінної
+X = data.drop('actual_label', axis=1)
 y = data['actual_label']
 
 # Розділяємо дані на тренувальний та тестовий набори
@@ -22,4 +23,8 @@ svm_model.fit(X_train, y_train)
 
 # Діагностичні метрики класифікації
 y_pred = svm_model.predict(X_test)
+
+# Рахуємо accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+
 print(classification_report(y_test, y_pred))
